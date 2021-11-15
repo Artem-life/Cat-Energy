6ac0d510bb7ba1ab971208254705431a2a32de57
Artem-life <72021798+Artem-life@users.noreply.github.com>
2021-11-15 23:07:43 +0300

    Add files via upload

---
+++ js/script-menu.js
@@ -0,0 +1,2 @@
+
+let NavMenuButton=document.querySelector(".page-header__wrapper-button");let MenuNav=document.querySelector(".nav-menu");NavMenuButton.onclick=function(){MenuNav.classList.toggle("nav-menu-block");};
